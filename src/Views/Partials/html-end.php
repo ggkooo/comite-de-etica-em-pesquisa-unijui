17638e29e5e2a41ce51dd829c710c64dbac98f6e
giordanoberwig <giordanoberwig@proton.me>
2025-03-03 15:16:47 -0300

    html-start, html-end and navbar | .php PARTIALS

---
+++ src/Views/Partials/html-end.php
@@ -0,0 +1,22 @@
+    <footer class="background-footer color-text-footer height-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-2 col-lg-2 text-center">
+                    <img src="/images/logo-unijui-branco.png" alt="Logo Unijui">
+                </div>
+                <div class="col-sm-12 col-md-8 col-lg-8 text-center">
+                    <p><a href="#" class="link-footer">Ouvidoria</a> | <a href="#" class="link-footer">Contatos</a> | <a href="#" class="link-footer">WebMail</a></p>
+                    <div>
+                        <p>Rua do Comércio, 3000, Bairro Universitário. <strong>CEP:</strong> 98700-000 | +55 (55) 3332 0200</p>
+                        <p><a href="#" class="link-footer">Ijuí</a> | <a href="#" class="link-footer">Santa Rosa</a> | <a href="#" class="link-footer">Panambi</a> | <a
+                                    href="#" class="link-footer">Três Passos</a></p>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-2 col-lg-2 text-center">
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
