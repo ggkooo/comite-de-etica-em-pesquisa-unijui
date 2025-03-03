9e672b229ca242e27db39848d19f61a31001d3be
giordanoberwig <giordanoberwig@proton.me>
2025-03-03 16:02:55 -0300

    Footer updated

--- src/Views/Partials/html-end.php
+++ src/Views/Partials/html-end.php
@@ -2,14 +2,15 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12 col-md-2 col-lg-2 text-center">
-                    <img src="/images/logo-unijui-branco.png" alt="Logo Unijui">
+                    <a href="https://www.unijui.edu.br/" target="_blank"><img src="/images/logo-unijui-branco.png" alt="Logo Unijui"></a>
                 </div>
                 <div class="col-sm-12 col-md-8 col-lg-8 text-center">
-                    <p><a href="#" class="link-footer">Ouvidoria</a> | <a href="#" class="link-footer">Contatos</a> | <a href="#" class="link-footer">WebMail</a></p>
+                    <p><a href="https://www.unijui.edu.br/institucional/ouvidoria" target="_blank" class="link-footer">Ouvidoria</a> | <a href="https://www.unijui.edu.br/contato" target="_blank" class="link-footer">Contatos</a> | <a href="https://workspace.google.com/intl/en-US/gmail/" target="_blank" class="link-footer">WebMail</a></p>
                     <div>
-                        <p>Rua do Comércio, 3000, Bairro Universitário. <strong>CEP:</strong> 98700-000 | +55 (55) 3332 0200</p>
-                        <p><a href="#" class="link-footer">Ijuí</a> | <a href="#" class="link-footer">Santa Rosa</a> | <a href="#" class="link-footer">Panambi</a> | <a
-                                    href="#" class="link-footer">Três Passos</a></p>
+                        <p>Rua do Comércio, 3000, Bairro Universitário. <strong>CEP:</strong> 98700-000 | <a
+                                    href="tel:+555533320200" target="_blank" class="link-footer color-text-footer">+55 (55) 3332 0200</a></p>
+                        <p><a href="https://www.unijui.edu.br/contato/campus-ijui" target="_blank" class="link-footer">Ijuí</a> | <a href="https://www.unijui.edu.br/contato/campus-santa-rosa" target="_blank" class="link-footer">Santa Rosa</a> | <a href="https://www.unijui.edu.br/contato/campus-panambi" target="_blank" class="link-footer">Panambi</a> | <a
+                                    href="https://www.unijui.edu.br/contato/campus-tres-passos" target="_blank" class="link-footer">Três Passos</a></p>
                     </div>
                 </div>
                 <div class="col-sm-12 col-md-2 col-lg-2 text-center">
