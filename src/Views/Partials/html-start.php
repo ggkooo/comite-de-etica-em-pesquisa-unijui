6fd72b5db99e02ce83d1126241dead08f0601ef2
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-03-03 15:16:47 -0300

    html-start, html-end and navbar | .php PARTIALS

---
+++ src/Views/Partials/html-start.php
@@ -0,0 +1,10 @@
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Comitê de Ética em Pesquisa - UNIJUÍ</title>
+    <link rel="stylesheet" href="/styles/style.css">
+    <link rel="stylesheet" href="/styles/footer.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
---
+++ src/Views/Partials/navbar.php
@@ -0,0 +1,49 @@
+<?php
+    $uri = $_SERVER['REQUEST_URI'];
+?>
+
+<nav class="navbar navbar-expand-xl bg-body-tertiary">
+    <div class="container-fluid mx-5">
+        <a class="navbar-brand bg-light" href="/"><img src="/images/logo-cep.png" alt="Logo unijui"></a>
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link <?php if ($uri == '/') {echo 'active';} ?>" aria-current="page" href="/">Início</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php if ($uri == '/membros') {echo 'active';} ?>" href="/membros">Membros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php if ($uri == '/normativas') {echo 'active';} ?>" href="/normativas">Normativas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php if ($uri == '/calendario') {echo 'active';} ?>" href="/calendario">Calendário de Reuniões</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Submissão de Projetos
+                    </a>
+                    <ul class="dropdown-menu bg-light">
+                        <li><a class="dropdown-item" href="/cadastro-plataforma-brasil">Cadastro na Plataforma Brasil</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="/plataforma-brasil">Plataforma Brasil</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php if ($uri == '/documentos') {echo 'active';} ?>" href="/documentos">Documentos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php if ($uri == '/duvidas') {echo 'active';} ?>" href="/duvidas">Dúvidas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contato">Contato</a>
+                </li>
+            </ul>
+            <span class="navbar-text">
+          </span>
+        </div>
+    </div>
+</nav>
