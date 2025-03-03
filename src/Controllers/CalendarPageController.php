9793fc72617e4ac89e0e83c0e8f82eb245004e34
giordanoberwig <giordanoberwig@proton.me>
2025-03-03 15:57:32 -0300

    Created the calendar page

---
+++ src/Controllers/CalendarPageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace giordanoberwig\ComiteDeEticaEmPesquisaUnijui\Controllers;
+
+class CalendarPageController
+{
+    public function __construct()
+    {
+        require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'calendar.php';
+    }
+}
