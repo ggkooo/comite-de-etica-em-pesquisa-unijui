48fe383b221937a62ac3b70f572431a2de645193
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-03-07 16:06:08 -0300

    Updated the members page

--- src/Views/members.php
+++ src/Views/members.php
@@ -1,1 +1,137 @@
 <?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'partials' . DIRECTORY_SEPARATOR . 'html-start.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'partials' . DIRECTORY_SEPARATOR . 'navbar.php';
+?>
+
+<div class="container text-justify lh-lg">
+    <div class="container">
+        <h2 class="font-primary">Coordenadores</h2>
+        <div class="container my-3 lh-slg">
+            <p class="font-primary fw-bold">Coordenador</p>
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/9828795111515673" target="_blank" rel="external">Prof. Dr. Aldemir Berwig 🔗</a>
+            <p>Formação: Direito e Administração</p>
+            <p>Doutorado em Educação nas Ciências - UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <p class="font-primary fw-bold">Coordenador-Adjunto</p>
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/7486006018113492" target="_blank" rel="external">Prof. Dr. Matias Nunes Frizzo 🔗</a>
+            <p>Formação: Farmácia</p>
+            <p>Doutorado em Biologia Celular e Molecular - PPGAIS/UNIJUÍ</p>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <h2 class="font-primary">Membros</h2>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/5894559134396184" target="_blank" rel="external">Prof. Dr. Airton Adelar Mueller 🔗</a>
+            <p>Formação: História</p>
+            <p>Doutorado em Sociologia - PPGDR/UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/4901428149058870" target="_blank" rel="external">Profa. Dra. Christiane de Fátima Colet 🔗</a>
+            <p>Formação: Farmácia</p>
+            <p>Doutorado em Ciências Farmacêuticas – PPGAIS/UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/2608690022743452" target="_blank" rel="external">Prof. Dr. Daniel Claudy da Silveira 🔗</a>
+            <p>Formação: Economia</p>
+            <p>Doutorado em Desenvolvimento Regional - UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/1332521470619712" target="_blank" rel="external">Prof. Dr. Doglas Cesar Lucas 🔗</a>
+            <p>Formação: Direito</p>
+            <p>Pós-Doutorado em Direito – PPGD/UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/9972212126180165" target="_blank" rel="external">Profa. Dra. Eliane Roseli Winkelmann 🔗</a>
+            <p>Formação: Fisioterapia</p>
+            <p>Pós-Doutorado em Ciências da Saúde – PPGAIS/UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/7034421658733755" target="_blank" rel="external">Prof. Dr. José Antonio Gonzalez da Silva 🔗</a>
+            <p>Formação: Agronomia</p>
+            <p>Doutorado em Agronomia – PPGSAS/UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/0716582715601931" target="_blank" rel="external">Profa. Dr. José Pedro Boufleuer 🔗</a>
+            <p>Formação: Filosofia</p>
+            <p>Doutorado em Educação – PPGEC/UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/0577622949754461" target="_blank" rel="external">Profa. Dra. Luciana Moro de Souza 🔗</a>
+            <p>Formação: Ciências Contábeis</p>
+            <p>Doutorado em Desenvolvimento Regional - UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/5669513153498812" target="_blank" rel="external">Profa. Dra. Marcia Regina Conceição de Almeida 🔗</a>
+            <p>Formação: Comunicação Social</p>
+            <p>Doutorado em Comunicação - UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/4998948009001946" target="_blank" rel="external">Profa. Dra. Moane Marchesan Krug 🔗</a>
+            <p>Formação: Fisioterapia e Educação Física</p>
+            <p>Doutorado em Educação Física - UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/6943517821737582" target="_blank" rel="external">Profa. Dra. Taciana Enderle 🔗</a>
+            <p>Formação: Engenharia Elétrica</p>
+            <p>Doutorado em Modelagem Matemática – PPGSAS/UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/7774743853922682" target="_blank" rel="external">Prof. Dr. Vitor Antunes de Oliveira 🔗</a>
+            <p>Formação: Biomedicina </p>
+            <p>Doutorado em Ciências Biológicas - Bioquímica Toxicológica - UNIJUÍ</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/7774743853922682" target="_blank" rel="external">Profa. Dra. Leonir Terezinha Uhde 🔗</a>
+            <p>Formação: Agronomia</p>
+            <p>Doutorado em Ciência do solo - UNIJUÍ</p>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <h2 class="font-primary">Representantes de Participante de Pesquisa</h2>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/4801096625531295" target="_blank" rel="external">Rosane Simon 🔗</a>
+            <p>Formação: Direito</p>
+            <p>Indicada pelo Conselho Municipal de Saúde de Ijuí</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/2637686261810040" target="_blank" rel="external">Marcia Andrea Ullmann 🔗</a>
+            <p>Formação: Nutrição</p>
+            <p>Indicada pelo Conselho Municipal de Saúde de Ijuí</p>
+        </div>
+        <hr>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/4919267670325919" target="_blank" rel="external">Eliana Elisa Rehfeld Gheno 🔗</a>
+            <p>Formação: Enfermagem e Obstetrícia</p>
+            <p>Mestrado em Atenção Integral à Saúde</p>
+            <p>Indicada pelo Conselho Municipal de Saúde de Ijuí</p>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <h2 class="font-primary">Secretária Responsável</h2>
+        <div class="container my-3 lh-slg">
+            <a class="font-primary-link text-decoration-none fw-bold" href="https://lattes.cnpq.br/6614074522180518" target="_blank" rel="external">Amália Iracema Pasche 🔗</a>
+            <p>Telefone: <a href="tel:+555533320301">(55) 3332-0301</a></p>
+        </div>
+    </div>
+</div>
+
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'Partials' . DIRECTORY_SEPARATOR . 'html-end.php';
