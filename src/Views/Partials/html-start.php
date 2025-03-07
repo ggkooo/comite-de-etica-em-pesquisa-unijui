0e76425f70a56c605a62970110ab02591ad90490
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-03-07 16:07:02 -0300

    Updated the site logo and name

--- src/Views/Partials/html-start.php
+++ src/Views/Partials/html-start.php
@@ -1,8 +1,11 @@
+<!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Comitê de Ética em Pesquisa - UNIJUÍ">
     <title>Comitê de Ética em Pesquisa - UNIJUÍ</title>
+    <link rel="icon" href="/images/logo-cep.png">
     <link rel="stylesheet" href="/styles/style.css">
     <link rel="stylesheet" href="/styles/footer.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
