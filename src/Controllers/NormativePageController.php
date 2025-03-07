e267594c1813ddee038f5db23d9f3e07a02e886c
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-03-07 16:05:47 -0300

    Created the normative page and controller

---
+++ src/Controllers/NormativePageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace giordanoberwig\ComiteDeEticaEmPesquisaUnijui\Controllers;
+
+class NormativePageController
+{
+    public function __construct()
+    {
+        require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'normative.php';
+    }
+}
