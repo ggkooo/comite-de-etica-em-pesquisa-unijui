c1931a62bde2ad4b4c27d245bf54204fca0eb37b
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-03-15 18:34:40 -0300

    Created and updated front controllers

--- src/Controllers/InicioPageController.php
+++ src/Controllers/InicioPageController.php
@@ -2,10 +2,10 @@
 
 namespace giordanoberwig\ComiteDeEticaEmPesquisaUnijui\Controllers;
 
-class HomePageController
+class InicioPageController
 {
     public function __construct()
     {
-        require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'home.php';
+        require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'inicio.php';
     }
 }
