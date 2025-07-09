1d3d714f0a324ee98ecb378f50b3ef152843e8ba
giordanoberwig <giordanoberwig@proton.me>
2025-07-09 11:02:27 -0300

    feat: configure web server settings and update public entry points

--- public/index.php
+++ public/index.php
@@ -1,36 +1,55 @@
 <?php
 
-use giordanoberwig\ComiteDeEticaEmPesquisaUnijui\Controllers\{CalendarioPageController,
-    FAQPageController,
-    InicioPageController,
-    MembrosPageController,
-    NormativasPageController,
-    Page404Controller};
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-$uri =  $_SERVER['REQUEST_URI'];
+define('LARAVEL_START', microtime(true));
 
-require_once __DIR__ . DIRECTORY_SEPARATOR .'..' . DIRECTORY_SEPARATOR . 'vendor' .DIRECTORY_SEPARATOR . 'autoload.php';
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
-if ($uri == '/') {
-    new InicioPageController();
-} else if ($uri == '/membros') {
-    new MembrosPageController();
-} else if ($uri == '/normativas') {
-    new NormativasPageController();
-} else if ($uri == '/reunioes') {
-    new CalendarioPageController();
-} else if ($uri == '/videos-tutoriais') {
-    require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' .DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'tutoriais.php';
-} else if ($uri == '/documentos-modelos') {
-    require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'modelos-documentos.php';
-} else if ($uri == '/educa-ceps') {
-    require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'educa-ceps.php';
-} else if ($uri == '/duvidas') {
-    new FAQPageController();
-} else if ($uri == '/contato') {
-    exit();
-} else if ($uri == '/404') {
-    new Page404Controller();
-} else {
-    header('Location: /404');
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
